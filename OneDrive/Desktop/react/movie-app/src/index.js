62ccaa6348392577339d049838b3fd3fa3d81483
Gaurav Khede <gauravkhede1@gmail.com>
2022-06-24 04:45:16 +0530

    done till dispatching actions

--- OneDrive/Desktop/react/movie-app/src/index.js
+++ OneDrive/Desktop/react/movie-app/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 import './index.css';
 import App from './component/App';
+import movies from './reducers/index';
 
+
+const store=createStore(movies);
+console.log(store,'Before state');
+console.log(store.getState(),' store');
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'Superman'}]
+});
+console.log(store.getState(),'After state');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
